07989cdce3ebb2835ae36f0fb0681568db83ded1
root <root@LAPTOP-3FDDMDNC>
2023-03-12 23:01:07 -0700

    updated datases and cate estimation

--- datasets.py
+++ datasets.py
@@ -8,7 +8,7 @@ from sklearn.base import clone
 def fetch_data_generator(*, data, semi_synth=False, simple_synth=False,
                          scale=1, true_f=None, max_depth=3):
     '''
-    data: one of {'401k', 'criteo', 'welfare'}
+    data: one of {'401k', 'criteo', 'welfare', 'poverty', 'charitable', 'star'}
     semi_synth: whether to impute outcomes from a synthetic model
     simple_synth: if outcome model should be simple based on the `true_f` function or fitted from data
     scale: how much noise to add for synthetic data generation
@@ -33,9 +33,7 @@ def fetch_data_generator(*, data, semi_synth=False, simple_synth=False,
         mask = (X['inc'] > 0) & (X['inc'] >= np.percentile(X['inc'], 1))
         mask &= (X['inc'] <= np.percentile(X['inc'], 99))
         X, D, y = X[mask], D[mask], y[mask]
-        inds = np.arange(X.shape[0])
-        np.random.shuffle(inds)
-        X, D, y = X.iloc[inds], D[inds], y[inds]
+        groups = None
 
     elif data == 'criteo':
         
@@ -44,7 +42,8 @@ def fetch_data_generator(*, data, semi_synth=False, simple_synth=False,
         y = df['visit'].values
         D = df['treatment'].values
         X = df.drop(['treatment', 'conversion', 'visit', 'exposure'], axis=1)
-        
+        groups = None
+
     elif data == 'welfare':
 
         abtest = True
@@ -63,6 +62,62 @@ def fetch_data_generator(*, data, semi_synth=False, simple_synth=False,
         y = df['y'].values
         D = df['w'].values
         X = df.drop(['y', 'w'], axis=1)
+        groups = None
+    
+    elif data == 'poverty':
+
+        abtest = True
+        df = pd.read_csv('https://raw.githubusercontent.com/gsbDBI/ExperimentData/master/Poverty/carvalho2016.csv', na_values=-999)
+        df = df.dropna()
+        y = df['outcome.correct.ans.per.second'].values
+        D = df['treatment'].values
+        X = df.drop(["outcome.correct.ans.per.second","outcome.num.correct.ans",
+                     'outcome.response.time', 'treatment'], axis=1)
+        groups = None
+
+    elif data == 'charitable':
+
+        abtest = True
+        df = pd.read_csv('https://raw.githubusercontent.com/gsbDBI/ExperimentData/master/Charitable/ProcessedData/charitable_withdummyvariables.csv', na_values=-999)
+        df = df.drop(['treat_ratio2', 'treat_ratio3', 'treat_size25', 'treat_size50',
+                     'treat_size100', 'treat_sizeno', 'treat_askd1', 'treat_askd2', 'treat_askd3',
+                     'out_amountgive', 'out_changeamtgive'], axis=1)
+        df = df.dropna()
+        y = df['out_gavedum'].values
+        D = df['treatment'].values
+        X = df.drop(['treatment', 'out_gavedum'], axis=1)
+        groups = None
+    
+    elif data == 'star':
+
+        abtest = True
+        URL = 'https://raw.githubusercontent.com/gsbDBI/ExperimentData/master/Project%20STAR/STAR_Students.tab'
+        df = pd.read_csv(URL, delimiter='\t')
+        Xcols = ['gender', 'race', 'birthmonth', 'birthyear',
+                'gkschid', 'gksurban', 'gktgen',
+                'gktrace', 'gkthighdegree', 'gktcareer',
+                'gktyears', 'gkfreelunch', 'gkrepeat',
+                'gkspeced', 'gkspecin']
+        Dcols = ['gkclasstype']
+        ycols = ['gktreadss', 'gktmathss']
+        Gcols = ['gktchid']
+        df = df[Xcols + Dcols + ycols + Gcols]
+        df = df.dropna()
+        y = np.sum(df[ycols].values, axis=1) # total of reading and math scores
+        D = 1.0 * (df[Dcols].values.flatten() == 1) # is small class
+        X = df[Xcols]
+        groups = df[Gcols[0]].values
+
+    else:
+        raise AttributeError("Dataset name is invalid!")
+
+    # shuffle data
+    inds = np.arange(X.shape[0])
+    np.random.shuffle(inds)
+    X, D, y = X.iloc[inds], D[inds], y[inds]
+    if groups is not None:
+        groups = groups[inds]
+
 
     # for semi-synthetic data generation
     if semi_synth:
@@ -74,7 +129,7 @@ def fetch_data_generator(*, data, semi_synth=False, simple_synth=False,
                 return np.random.normal(0, scale * std, size=n)
 
             def get_data():
-                return X, D, true_f(D, X) + gen_epsilon(X.shape[0])
+                return X, D, true_f(D, X) + gen_epsilon(X.shape[0]), groups
 
         else:
 
@@ -88,12 +143,12 @@ def fetch_data_generator(*, data, semi_synth=False, simple_synth=False,
                 return scale * np.random.choice(true_residuals, size=n)
 
             def get_data():
-                return X, D, true_f(D, X) + gen_epsilon(X.shape[0])
+                return X, D, true_f(D, X) + gen_epsilon(X.shape[0]), groups
     
     else:
 
         def get_data():
-            return X, D, y
+            return X, D, y, groups
         
         def true_f(D, X):
             return np.zeros(D.shape[0])
